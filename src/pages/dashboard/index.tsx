8b8d80ca7d3dc460b71d1d1b653d6edf33ef7932
gilmarferrini <ferrinigilmar@gmail.com>
2020-09-07 10:13:27 -0300

    first commit

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explorer repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
+};
+
+export default Dashboard;
