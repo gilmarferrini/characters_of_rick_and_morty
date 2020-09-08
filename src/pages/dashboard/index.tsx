4dd8fe255b5e9d6492dde46adc9feb094064848c
ferrinigilmar <ferrinigilmar@gmail.com>
2020-09-08 08:27:17 -0300

    refactoring

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -12,6 +12,21 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51804511?s=460&u=9cc1b4f147cbf4cdf51e3487948cbc6663a30123&v=4"
+            alt=""
+          />
+          <div>
+            <strong>Facebook/react</strong>
+            <p>
+              React is an open source JavaScript used for designing user
+              interfaces
+            </p>
+          </div>
+        </a>
+      </Repositories>
     </>
   );
 };
