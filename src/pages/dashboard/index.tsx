749c5c1934f46dc1af1575f7475439ccfb9410bf
ferrinigilmar <ferrinigilmar@gmail.com>
2020-09-08 08:54:31 -0300

    refactoring

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'; // feather icons
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
@@ -16,7 +17,7 @@ const Dashboard: React.FC = () => {
         <a href="/">
           <img
             src="https://avatars0.githubusercontent.com/u/51804511?s=460&u=9cc1b4f147cbf4cdf51e3487948cbc6663a30123&v=4"
-            alt=""
+            alt="Gilmar Ferrini"
           />
           <div>
             <strong>Facebook/react</strong>
@@ -25,6 +26,37 @@ const Dashboard: React.FC = () => {
               interfaces
             </p>
           </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51804511?s=460&u=9cc1b4f147cbf4cdf51e3487948cbc6663a30123&v=4"
+            alt="Gilmar Ferrini"
+          />
+          <div>
+            <strong>Facebook/react</strong>
+            <p>
+              React is an open source JavaScript used for designing user
+              interfaces
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51804511?s=460&u=9cc1b4f147cbf4cdf51e3487948cbc6663a30123&v=4"
+            alt="Gilmar Ferrini"
+          />
+          <div>
+            <strong>Facebook/react</strong>
+            <p>
+              React is an open source JavaScript used for designing user
+              interfaces
+            </p>
+          </div>
+          <FiChevronRight size={20} />
         </a>
       </Repositories>
     </>
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -59,6 +59,15 @@ export const Repositories = styled.div`
 
     display: flex;
     align-items: center;
+    transition: 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
 
     img {
       width: 64px;
@@ -79,6 +88,11 @@ export const Repositories = styled.div`
         color: #a8a8b3;
         margin-top: 4px;
       }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
